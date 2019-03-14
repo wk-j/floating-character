e02f86e3f527ca22c52c027f89c2252e0af48217
wk-j <somnuk.wk@gmail.com>
2019-03-15 01:14:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Floating Character
+
+[![NuGet](https://img.shields.io/nuget/v/wk.FloatingCharacter.svg)](https://www.nuget.org/packages/wk.FloatingCharacter)
+
+```
+dotnet add package wk.FloatingCharacter
+```
---
+++ src/FloatingCharacter/Glyph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace FloatingCharacter {
+    public class Glyph {
+        static char[] floatingChars = "  ั   ำ   ิ  ี  ึ  ื  ุ  ู  ่  ้   ๊  ๋   ์  "
+            .Where(x => x != ' ').ToArray();
+
+        public static char[] FloatingCharacters() => floatingChars;
+    }
+}
