084e855f6c27d1da5128b6dce54598a6ba4218b9
wk-j <somnuk.wk@gmail.com>
2019-03-16 21:36:53 +0700

    Update Glyph.cs

--- src/FloatingCharacter/Glyph.cs
+++ src/FloatingCharacter/Glyph.cs
@@ -4,10 +4,10 @@ using System.Linq;
 namespace FloatingCharacter {
     public class Glyph {
 
-        public static char[] FloatingCharacters = "  ั  ็  ิ  ี  ึ  ื  ุ  ู  ่  ้   ๊  ๋   ์  "
+        public static char[] FloatingCharacters = "  ั  ็  ิ ำ ี  ึ  ื  ุ  ู  ่  ้   ๊  ๋   ์  "
             .Where(x => x != ' ').ToArray();
         
-        public static char[] IgnoreCharacters = "  ั  ็  ิ  ี  ึ  ื  ่  ้   ๊  ๋  ์  "
+        public static char[] IgnoreCharacters = "  ั  ็  ิ  ี  ึ  ื  ุ  ู ่  ้   ๊  ๋  ์  "
             .Where(x => x != ' ').ToArray();
     }
 }
